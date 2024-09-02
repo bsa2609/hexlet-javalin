d1da09902bc8e370d603fd2cbc2c79adee3c7687
Sergey Breusov <bsa2609@yandex.ru>
2024-09-02 11:51:27 +0300

    create build. gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,16 @@
+import org.gradle.api.tasks.testing.logging.TestExceptionFormat
+import org.gradle.api.tasks.testing.logging.TestLogEvent
+
 plugins {
     id("java")
+    application
 }
 
-group = "hexlet.code"
+application {
+    mainClass.set("org.example.hexlet.HelloWorld")
+}
+
+group = "org.example"
 version = "1.0-SNAPSHOT"
 
 repositories {
@@ -10,10 +18,27 @@ repositories {
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
+    // Версии зависимостей могут отличаться
+    // Здесь мы сразу подключаем зависимости,
+    // которые понадобятся во время обучения
+    implementation("io.javalin:javalin:6.2.0")
+    implementation("org.slf4j:slf4j-simple:2.0.13")
+    implementation("io.javalin:javalin-rendering:6.1.6")
+    implementation("gg.jte:jte:3.1.12")
+//    testImplementation(platform("org.junit:junit-bom:5.9.1"))
+//    testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.11.0-M1")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
 tasks.test {
     useJUnitPlatform()
+    // https://technology.lastminute.com/junit5-kotlin-and-gradle-dsl/
+    testLogging {
+        exceptionFormat = TestExceptionFormat.FULL
+        events = mutableSetOf(TestLogEvent.FAILED, TestLogEvent.PASSED, TestLogEvent.SKIPPED)
+        // showStackTraces = true
+        // showCauses = true
+        showStandardStreams = true
+    }
 }
